9bcff3baa1302460d084c76d7db9fc322b01a51d
Caine <24969357+CaineArdayfio@users.noreply.github.com>
2023-01-06 03:46:22 -0300

    f

--- src/App.js
+++ src/App.js
@@ -4,28 +4,6 @@ import './App.css';
 import { useRef } from 'react'
 import handleSubmit from './handles/handlesubmit';
 
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
 function App() {
   const dataRef = useRef()
 
