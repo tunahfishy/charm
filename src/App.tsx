0bd67b9e3069438d9a33df76f06a755aec5ce95a
Caine <24969357+CaineArdayfio@users.noreply.github.com>
2023-01-07 13:42:58 -0300

    auth stuff working

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import handleSubmit from "./handles/handlesubmit";
 import React from 'react';
 
@@ -15,8 +15,27 @@ function App() {
     }
   };
 
+  const get_auth_url = async () => {
+    fetch("http://localhost:4000/auth/get_auth_url").then((res) => res.text()).then((url) => {
+      // redirect the user
+      window.open(url, '_blank', 'noreferrer');
+    })
+  }
+
+  // send a GET request to the server
+  useEffect(() => {
+
+    /*
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+      */
+  }, []);
+
   return (
     <div className="App">
+      <button onClick={() => {
+          get_auth_url()
+        }}>Authorize Google</button>
       <form onSubmit={submitHandler}>
         <input type="text" ref={dataRef} />
         <button type="submit">Save</button>
