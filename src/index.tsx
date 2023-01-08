828513cf2c5904ae468caf57349b1b0d9c069875
Tinah <tinahh@mit.edu>
2023-01-08 11:02:37 -0300

    add nextui

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Email from "./pages/email"
-// import AddPhoto from "./addPhoto";
-// import GenerateText from "./generateText";
+import Email from "./pages/email";
+import Profiles from "./pages/profiles";
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
+import { NextUIProvider } from "@nextui-org/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/email" element={<Email/>}/>
-        {/* <Route path="photos" element={<AddPhoto />} />
-        <Route path="email" element={<GenerateText />} /> */}
-        <Route path="*" element={<h1>Route does not exist</h1>} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <NextUIProvider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/email" element={<Email />} />
+          <Route path="/profiles" element={<Profiles />} />
+          <Route path="*" element={<h1>Route does not exist</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  </NextUIProvider>
 );
